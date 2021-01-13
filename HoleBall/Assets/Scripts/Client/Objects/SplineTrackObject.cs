af734ad4766654ebe9f9a871634cc64c354651ad
Vladimir Makarov <v.makarov@pixonic.com>
2021-01-13 18:33:53 +0300

    splines

---
+++ HoleBall/Assets/Scripts/Client/Objects/SplineTrackObject.cs
@@ -0,0 +1,25 @@
+using System;
+using DG.Tweening;
+using Dreamteck.Splines;
+using UnityEngine;
+
+namespace Client.Objects
+{
+    public class SplineTrackObject : MonoBehaviour
+    {
+        public SplineComputer Spline;
+        public float TrackingDuration;
+
+        public void StartTracking(GameObject go, float start, float end)
+        {
+            var currentValue = start;
+            DOTween.Sequence().AppendCallback(() => go.GetComponent<Rigidbody>().isKinematic = true).
+                Append(DOTween.To(value =>
+                    {
+                        currentValue = value;
+                        go.transform.position = Spline.EvaluatePosition(value);
+                    }, start, end, TrackingDuration))
+                .AppendCallback(() => go.GetComponent<Rigidbody>().isKinematic = false);
+        }
+    }
+}
